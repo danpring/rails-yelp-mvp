4985a39c9822579554ffe8afc47b3827c4223252
Dan Ring <dan.ring@me.com>
2021-01-14 19:49:12 +0900

    completed yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: rand(0..10).to_s
+  )
+end
+
+puts "Done!"
